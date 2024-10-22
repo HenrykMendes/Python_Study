b69f5b2a1ecea199f7f478b812e8f50294083711
João Henrique Mendes <h06.987654321@gmail.com>
2024-10-22 16:53:47 -0300

    Estudo de python 22/10

--- Estudos_Python/unimed.py
+++ Estudos_Python/unimed.py
@@ -1,3 +1,14 @@
-msg = "Hello World"
-print (msg)
-print ("Olá mundo")
+name = 'joão Henrique'
+age = 21
+
+print (f"Meu nome é: {name}")
+print (f"Tenho: {age} anos")
+
+Linguagens = [
+    'Java',
+    'html',
+    'css',
+    'Python'
+]
+
+print (Linguagens)
