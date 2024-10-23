546e5b7ecbad65aaa772991df683b1ea2030fa1c
João Henrique Mendes <h06.987654321@gmail.com>
2024-10-23 17:03:55 -0300

    Atividades de Python

---
+++ Estudos_Python/lista_de_exercicios/01_estrutura_sequencial/CelciusParaFahreheint.py
@@ -0,0 +1,4 @@
+#Faça um Programa que peça a temperatura em graus Celsius, transforme e mostre em graus Fahrenheit.
+temperatura_celcius = int(input("Digite o valor em Celcius: "))
+temperatura_fareheit = (temperatura_celcius * 1.8) +32
+print (temperatura_fareheit)
---
+++ Estudos_Python/lista_de_exercicios/02_Estrutura_De_Decisao/Imprimir_maior_valor.py
@@ -0,0 +1,11 @@
+#Faça um Programa que peça dois números e imprima o maior deles.
+numero_01 =  int (input('digite: '))
+numero_02 = int (input ('digite: '))
+
+#identação: forma de organização
+#na estuura condicional sem uso de chaves e parentesês
+#depois da condição do if, else ou if else, usar dois pontos
+if numero_01 > numero_02:
+    print ('Numero 01 é maior')
+else: 
+    print('Numero 02 é maior')
