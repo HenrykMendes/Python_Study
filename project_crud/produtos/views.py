643cd1b258b3f957d012f132dad49038fbfb450f
João Henrique Mendes <h06.987654321@gmail.com>
2024-10-25 17:33:58 -0300

    .

--- project_crud/produtos/views.py
+++ project_crud/produtos/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.shortcuts import render, redirect, get_object_or_404
+#importando o arquivo models da pasta produtos
+#importando ProdutosForms da pasta forms
+from .models import Produtos 
+from .forms import ProdutosForm
+
+
+def lista_produtos(request):
+    
