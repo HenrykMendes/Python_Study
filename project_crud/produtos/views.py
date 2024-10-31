b4a8c310cfa04eb120ea75b89e751a8231ff0649
João Henrique Mendes <h06.987654321@gmail.com>
2024-10-31 17:56:40 -0300

    30/10 Atualização da URLs e Views do CRUD Django

--- project_crud/produtos/views.py
+++ project_crud/produtos/views.py
@@ -1,5 +1,4 @@
 from django.shortcuts import render
-
 # Create your views here.
 from django.shortcuts import render, redirect, get_object_or_404
 #importando o arquivo models da pasta produtos
@@ -7,6 +6,31 @@ from django.shortcuts import render, redirect, get_object_or_404
 from .models import Produtos 
 from .forms import ProdutosForm
 
-
+#lista de produtos
 def lista_produtos(request):
+    produto = Produtos.objects.all()
+    return render (request, 'produto/lista_produtos.html') #criar a página html e retornar a página como resposta
+
+#criar um produto
+def criar_produto (request):
+    form = ProdutosForm(request.POST or None)
+    if form.is_valid():
+        form.save()
+        return redirect ('lista_produtos')
     
+#editar um produto
+def editar_produto (request, id):
+    produto = get_object_or_404(Produtos, id = id)
+    form = ProdutosForm (request.POST)
+    if form.is_valid():
+       form.save()
+       return redirect ('lista_produtos')
+    return render (request,'produtos/editar_produto', {'form':form, 'produto', produto})
+
+#deletar produto
+def deletar_produto (request, id):
+    produto = get_object_or_404 (Produtos,id=id)
+    if request.method == 'POST':
+        produto.delete()
+        return redirect('lista_produtos')
+    return render (request, 'produtos/deletar_produto.html', {'produto' : produto})
