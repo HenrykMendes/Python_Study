fb1da3d4362281cdb695a219ca0ebe14f8307752
J. Henrique Mendes <h06.987654321@gmail.com>
2024-11-02 17:44:20 -0300

    Exclusão dos arquivos do Django

--- project_crud/produtos/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-from .models import Produtos
-
-class ProdutosForm (forms.ModelForm):
-    class meta:
-        model = Produtos
-        fields = ['nome', 'descricao', 'preco', 'estoque']
